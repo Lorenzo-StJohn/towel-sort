50a7d5613b0b5c17c42e6e1c8d7fee82060e7986
Lorenzo-StJohn <britishaccent@bk.ru>
2026-04-27 09:54:51 +0400

    refactor: apply linter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-module.exports = function towelSort (matrix) {
-    let result = [];
-    if (!matrix) return [];
-    for (let i = 0; i < matrix.length; ++i) {
-        if (i % 2 === 0) {
-            if (matrix[i]) for (let j = 0; j <matrix[i].length; ++j) result.push(matrix[i][j]);
-        }
-        else {
-            if (matrix[i]) for (let j = matrix[i].length - 1;  j >= 0; --j) result.push(matrix[i][j]);
-        }
-    }
+module.exports = function towelSort(matrix) {
+  const result = [];
+  if (!matrix) return [];
+  for (let i = 0; i < matrix.length; i += 1) {
+    if (i % 2 === 0) {
+      if (matrix[i])
+        for (let j = 0; j < matrix[i].length; j += 1) result.push(matrix[i][j]);
+    } else if (matrix[i])
+      for (let j = matrix[i].length - 1; j >= 0; j -= 1)
+        result.push(matrix[i][j]);
+  }
   return result;
-}
+};
